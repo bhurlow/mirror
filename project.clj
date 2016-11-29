803eb6cc0556b2abc1f6eed3e74e56ffb6be4807
Brian Hurlow <brian@brianhurlow.com>
2016-11-29 14:52:16 -0500

    start

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject mirror "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cljsjs/react "15.3.1-0"]
+                 [cljsjs/react-dom "15.3.1-0"]])
---
+++ src/mirror/core.clj
@@ -0,0 +1,6 @@
+(ns mirror.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
