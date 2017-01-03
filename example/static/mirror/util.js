8a10508cda40d057e5e1f71ffb61e04b42dc8373
Brian Hurlow <brian@brianhurlow.com>
2017-01-03 05:35:24 -0800

    git ignore generated static files

--- example/static/mirror/util.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.293 {}
-goog.provide('mirror.util');
-goog.require('cljs.core');
-
-//# sourceMappingURL=util.js.map
--- example/static/reagent/interop.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.293 {}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
-
-//# sourceMappingURL=interop.js.map
