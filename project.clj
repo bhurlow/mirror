03016ce36f44e2eccb0b82ec217d5912ea96b69d
Brian Hurlow <brian@brianhurlow.com>
2024-09-07 16:55:50 -0400

    bump deps to fix compiler

--- project.clj
+++ project.clj
@@ -1,21 +1,20 @@
-(defproject mirror "0.2.1"
+(defproject mirror "0.3.0"
   :description "isomorphic clojure/script web apps"
   :main mirror.core
   :url "https://github.com/bhurlow/mirror"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [aleph "0.4.1-beta7"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [aleph "0.8.1"]
                  ;; https://github.com/bhurlow/mirror/issues/14
                  ;; reagent 0.6.0 breaks stuff... ?? 
                  [reagent "0.5.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [hiccup "1.0.5"]
-                 [manifold "0.1.5"]
+                 [org.clojure/clojurescript "1.11.132"]
+                 [hiccup "2.0.0-RC3"]
                  [me.raynes/fs "1.4.6"]
                  [mvxcvi/puget "1.0.1"]
-                 [ring "1.4.0"]]
+                 [ring/ring-core "1.12.2"]]
   :uberjar-name "mirror.jar"
   :profiles {:dev {:source-paths  ["dev" "src" "test" "example"]}
              :uberjar {:main mirror.cli}})
